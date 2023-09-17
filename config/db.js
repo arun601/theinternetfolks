bb59a6470e35ae56b3eb1db83363b9fae9335f21
Arun Yadav <78892809+arun601@users.noreply.github.com>
2023-09-17 13:08:10 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://arunyadav838481:838481ay@cluster0.b8jzxhq.mongodb.net/Internetfolks?retryWrites=true&w=majority");
+    const conn = await mongoose.connect("mongodb+srv?/@cluster0.b8jzxhq.mongodb.net/Internetfolks?retryWrites=true&w=majority");
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`
     );
